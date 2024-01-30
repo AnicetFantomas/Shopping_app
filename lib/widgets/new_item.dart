1ede40f2fbd83e02b8570b72f509ebedc5b70c50
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-30 17:02:37 +0200

    Display dummy data

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -9,6 +9,12 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemStateState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,71 +24,79 @@ class _NewItemStateState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+            key: _formKey,
             child: Column(
-          children: [
-            TextFormField(
-              maxLength: 50,
-              decoration: const InputDecoration(label: Text('Name')),
-              validator: (value) {
-                if (value == null ||
-                    value.isEmpty ||
-                    value.trim().length <= 1 ||
-                    value.trim().length > 50) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(label: Text('Quantity')),
-                    initialValue: '1',
-                    validator: (value) {
-                      if (value == null ||
-                          value.isEmpty ||
-                          int.tryParse(value) == null ||
-                          int.tryParse(value)! <= 0) {
-                        return 'Please enter a quantity greater than 0';
-                      }
-                      return null;
-                    },
-                  ),
+                TextFormField(
+                  maxLength: 50,
+                  decoration: const InputDecoration(label: Text('Name')),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
                 ),
-                const SizedBox(
-                  width: 8,
-                ),
-                Expanded(
-                  child: DropdownButtonFormField(items: [
-                    for (final category in categories.entries)
-                      DropdownMenuItem(
-                        value: category.value,
-                        child: Row(children: [
-                          Container(
-                            width: 16,
-                            height: 16,
-                            color: category.value.color,
-                          ),
-                          const SizedBox(width: 6),
-                          Text(category.value.name),
-                        ]),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(label: Text('Quantity')),
+                        keyboardType: TextInputType.number,
+                        initialValue: '1',
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              int.tryParse(value) == null ||
+                              int.tryParse(value)! <= 0) {
+                            return 'Please enter a quantity greater than 0';
+                          }
+                          return null;
+                        },
                       ),
-                  ], onChanged: (value) {}),
-                )
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Expanded(
+                      child: DropdownButtonFormField(items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(category.value.name),
+                            ]),
+                          ),
+                      ], onChanged: (value) {}),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          _formKey.currentState!.reset();
+                        },
+                        child: const Text('Reset')),
+                    ElevatedButton(
+                        onPressed: _saveItem, child: const Text('Add Item')),
+                  ],
+                ),
               ],
-            ),
-            const SizedBox(height: 12),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(onPressed: () {}, child: const Text('Cancel')),
-                ElevatedButton(onPressed: () {}, child: const Text('Add Item')),
-              ],
-            ),
-          ],
-        )),
+            )),
       ),
     );
   }
