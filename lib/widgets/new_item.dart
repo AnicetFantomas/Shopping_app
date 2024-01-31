e3ac14285444bf18f1a42f103390fe8ae97f7a76
AnicetFantomas <anicetmurhula20@gmail.com>
2024-01-31 13:00:49 +0200

    Send a post request to firebase

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,14 +19,14 @@ class _NewItemStateState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
           'flutter-shopping-8000a-default-rtdb.firebaseio.com',
           'shopping-list.json');
 
-      http.post(
+     final response = await http.post(
         url,
         headers: {'Content-Type': 'application/json'},
         body: json.encode({
@@ -36,12 +36,14 @@ class _NewItemStateState extends State<NewItem> {
         }),
       );
 
-      // Navigator.of(context).pop(GroceryItem(
-      //   id: DateTime.now().toString(),
-      //   name: _enteredName,
-      //   quantity: _enteredQuantity,
-      //   category: _selectedCategory,
-      // ));
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop();
     }
   }
 
